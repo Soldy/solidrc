aecf3fa58855faca1c27b5c4ee48f69043c22d19
Soldy <4786022+Soldy@users.noreply.github.com>
2021-02-17 20:02:31 +0000

    interface design
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,76 @@
+
+const fs = require('fs');
+
+
+const solidBase = function(){
+    let fd;
+    let dataSize;
+    let dataCount;
+    let dataDescription;
+    let dataDescriptionSize;
+    const open = function(){
+        
+         
+    }
+    const writeCard = function(){
+
+    }
+    const readCard = function(){
+
+    }
+    const writeStatus = function(){
+
+    }
+    const readStatus = function(){
+
+    }
+    const writeSize = function(){
+
+    }
+    const readSize = function(){
+
+    }
+    const writeCount = function(){
+
+    }
+    const readCount = function(){
+
+    }
+    const countCount = function(){
+
+    }
+
+    const writeDescription = function(){
+
+    }
+    const readDescription = function(){
+
+    }
+
+    const close = function(){
+    }
+    const readBlock = function(){
+
+    }
+    const writeBlock = function(){
+
+    }
+    const addBlock = function(){
+
+    }
+
+}
+
+
+
+
+/*
+   5 byte   | s0l1d
+   UInt8    | status
+   UInt32   | block size
+   UInt32   | block count
+   UInt32   | creation time stamp
+   UInt16   | descriptin size
+
+
+*/
